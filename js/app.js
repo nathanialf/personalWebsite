c7e9a5bee6bd313343480a69232e883723853733
nathanialf <nathanial.fine@gmail.com>
2018-09-02 19:42:24 -0700

    add more information

--- js/app.js
+++ js/app.js
@@ -8,5 +8,8 @@ app.config(function($routeProvider, $locationProvider){
 	}).when("/",{
 		templateUrl: "pages/home.html",
 		controller: "homeController"
+	}).when("/personal",{
+		templateUrl: "pages/personal.html",
+		controller: "personalController"
 	}).otherwise({redirectTo: "/"});
 });
